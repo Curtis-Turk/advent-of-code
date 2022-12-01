14d98dc8b2453aa84fdddc7dec4a77e70b0e5d36
Curtis Turk <curtis.c.turk@outlook.com>
2022-12-01 21:06:11 +0000

    test for single elf 3 meals

--- day1/calorieCounting.js
+++ day1/calorieCounting.js
@@ -3,10 +3,12 @@ const fs = require("fs");
 const inputData = fs.readFileSync("./input.txt", "utf-8");
 
 const calorieCounting = (inputData) => {
-  console.log(inputData.split("\n"));
-  return [parseInt(inputData)];
+  return [
+    inputData
+      .split("\n")
+      .map(Number)
+      .reduce((a, b) => a + b),
+  ];
 };
-
-calorieCounting(inputData);
 
 module.exports = calorieCounting;
--- day1/calorieCounting.test.js
+++ day1/calorieCounting.test.js
@@ -1,5 +1,11 @@
 const calorieCounting = require("./calorieCounting");
 
-test("returns a value", () => {
+test("returns a value for one elf", () => {
   expect(calorieCounting("1")).toEqual([1]);
+});
+test("returns a value for one elf 2 meals", () => {
+  expect(calorieCounting("1\n1")).toEqual([2]);
+});
+test("returns a value for one elf 3 meals", () => {
+  expect(calorieCounting("1\n1\n1")).toEqual([3]);
 });
