821c0753a550bfae77a4316d6f29220e6887b308
Curtis Turk <curtis.c.turk@outlook.com>
2022-12-02 11:29:56 +0000

    Passing tests for multiple elves

--- day1/calorieCounting.js
+++ day1/calorieCounting.js
@@ -3,12 +3,24 @@ const fs = require("fs");
 const inputData = fs.readFileSync("./input.txt", "utf-8");
 
 const calorieCounting = (inputData) => {
-  return [
-    inputData
-      .split("\n")
-      .map(Number)
-      .reduce((a, b) => a + b),
-  ];
+  const toNum = inputData.split("\n").map(Number);
+
+  let elfArray = [0];
+  let elfIndex = 0;
+  let currentElfIndex = 0;
+
+  for (let i = 0; i < toNum.length; i++) {
+    if (!toNum[i]) {
+      elfIndex++;
+      elfArray[elfIndex] = 0;
+      currentElfIndex = 0;
+      continue;
+    }
+
+    elfArray[elfIndex] += toNum[i];
+  }
+
+  return elfArray;
 };
 
 module.exports = calorieCounting;
--- day1/calorieCounting.test.js
+++ day1/calorieCounting.test.js
@@ -1,11 +1,22 @@
 const calorieCounting = require("./calorieCounting");
 
-test("returns a value for one elf", () => {
-  expect(calorieCounting("1")).toEqual([1]);
+describe("Single elf", () => {
+  test("returns a value for one meal", () => {
+    expect(calorieCounting("1")).toEqual([1]);
+  });
+  test("returns a value for 2 meals", () => {
+    expect(calorieCounting("1\n1")).toEqual([2]);
+  });
+  test("returns a value for 3 meals", () => {
+    expect(calorieCounting("1\n1\n1")).toEqual([3]);
+  });
 });
-test("returns a value for one elf 2 meals", () => {
-  expect(calorieCounting("1\n1")).toEqual([2]);
-});
-test("returns a value for one elf 3 meals", () => {
-  expect(calorieCounting("1\n1\n1")).toEqual([3]);
+
+describe("2 elves", () => {
+  test("returns a value for one meal each", () => {
+    expect(calorieCounting("1\n\n1")).toEqual([1, 1]);
+  });
+  test("returns a value for many meals", () => {
+    expect(calorieCounting("1\n1\n1\n\n1\n1")).toEqual([3, 2]);
+  });
 });
