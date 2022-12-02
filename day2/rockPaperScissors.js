3a2dfac60877fe3435fd83dea4fd309107375695
Curtis Turk <curtis.c.turk@outlook.com>
2022-12-02 13:35:45 +0000

    Passing for draws

--- day2/rockPaperScissors.js
+++ day2/rockPaperScissors.js
@@ -3,7 +3,12 @@ const fs = require("fs");
 const inputData = fs.readFileSync("./input.txt", "utf-8").split("\n");
 
 const rockPaperScissors = (inputData) => {
-  console.log(inputData);
+  // console.log({ inputData });
+  const me = inputData[0][0];
+  // const opp = inputData[2];
+
+  console.log(me);
+  return rpcScores[me] + 3;
 };
 
 // rockPaperScissors(inputData);
@@ -12,8 +17,11 @@ let rpcScores = {
   A: 1,
   B: 2,
   C: 3,
+  X: 1,
+  Y: 2,
+  Z: 3,
 };
 
-console.log(rpcScores["A"]);
+// console.log(rpcScores["A"]);
 
 module.exports = rockPaperScissors;
--- day2/rockPaperScissors.test.js
+++ day2/rockPaperScissors.test.js
@@ -1,3 +1,13 @@
 const rockPaperScissors = require("./rockPaperScissors");
 
-test("");
+describe("single round", () => {
+  test("draw with rock", () => {
+    expect(rockPaperScissors(["A X"])).toEqual(4);
+  });
+  test("draw with paper", () => {
+    expect(rockPaperScissors(["B Y"])).toEqual(5);
+  });
+  test("draw with scissors", () => {
+    expect(rockPaperScissors(["C Z"])).toEqual(6);
+  });
+});
