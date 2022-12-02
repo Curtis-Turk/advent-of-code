b093e8a98bf299c4202358db2239318fa88649c7
Curtis Turk <curtis.c.turk@outlook.com>
2022-12-02 11:39:15 +0000

    completed part 2

--- day1/calorieCounting.js
+++ day1/calorieCounting.js
@@ -3,24 +3,28 @@ const fs = require("fs");
 const inputData = fs.readFileSync("./input.txt", "utf-8");
 
 const calorieCounting = (inputData) => {
-  const toNum = inputData.split("\n").map(Number);
+  const elfCalorieData = inputData.split("\n").map(Number);
 
   let elfArray = [0];
   let elfIndex = 0;
   let currentElfIndex = 0;
 
-  for (let i = 0; i < toNum.length; i++) {
-    if (!toNum[i]) {
+  for (let i = 0; i < elfCalorieData.length; i++) {
+    if (!elfCalorieData[i]) {
       elfIndex++;
       elfArray[elfIndex] = 0;
       currentElfIndex = 0;
       continue;
     }
 
-    elfArray[elfIndex] += toNum[i];
+    elfArray[elfIndex] += elfCalorieData[i];
   }
 
   return elfArray;
 };
+
+sortedElves = calorieCounting(inputData).sort((a, b) => b - a);
+
+console.log(sortedElves[0] + sortedElves[1] + sortedElves[2]);
 
 module.exports = calorieCounting;
