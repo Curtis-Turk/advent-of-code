2a7ea52195417f3b01efbdc8e22342bd6041d505
Curtis Turk <curtis.c.turk@outlook.com>
2022-12-05 15:55:11 +0000

    part two answer correct

--- day2/rockPaperScissors.js
+++ day2/rockPaperScissors.js
@@ -2,11 +2,7 @@ const fs = require("fs");
 
 const inputData = fs.readFileSync("./input.txt", "utf-8").split("\n");
 
-const rockPaperScissors = (inputData) => {
-  return inputData.map((round) => scoreRound(round)).reduce((a, b) => a + b);
-};
-
-const scoreRound = (round) => scoreObj[round] + myChoice[round[2]];
+// PART ONE
 
 const scoreObj = {
   "A X": 3,
@@ -26,8 +22,43 @@ const myChoice = {
   Z: 3,
 };
 
+const scoreRound = (round) => scoreObj[round] + myChoice[round[2]];
+
+const rockPaperScissors = (inputData) => {
+  return inputData.map((round) => scoreRound(round)).reduce((a, b) => a + b);
+};
+
+//PART TWO
+
+// A rock = 1 / B paper = 2 / C scissors = 3
+// X lose / Y draw / Z win
+const strategyObj = {
+  "A Y": 1,
+  "B Y": 2,
+  "C Y": 3,
+  "A X": 3,
+  "B X": 1,
+  "C X": 2,
+  "A Z": 2,
+  "B Z": 3,
+  "C Z": 1,
+};
+
+const winObj = {
+  X: 0,
+  Y: 3,
+  Z: 6,
+};
+
+const scoreStrategy = (round) => strategyObj[round] + winObj[round[2]];
+
+const fullStrategy = (inputData) => {
+  return inputData.map((round) => scoreStrategy(round)).reduce((a, b) => a + b);
+};
+
 // part 1
 console.log(rockPaperScissors(inputData));
 // part 2
+console.log(fullStrategy(inputData));
 
-module.exports = rockPaperScissors;
+module.exports = { rockPaperScissors, fullStrategy };
--- day2/rockPaperScissors.test.js
+++ day2/rockPaperScissors.test.js
@@ -1,66 +1,84 @@
-const rockPaperScissors = require("./rockPaperScissors");
+const { rockPaperScissors, fullStrategy } = require("./rockPaperScissors");
 
-describe("single round", () => {
-  test("draw with rock", () => {
-    expect(rockPaperScissors(["A X"])).toEqual(4);
+describe("part 1", () => {
+  describe("single round", () => {
+    test("draw with rock", () => {
+      expect(rockPaperScissors(["A X"])).toEqual(4);
+    });
+    test("draw with paper", () => {
+      expect(rockPaperScissors(["B Y"])).toEqual(5);
+    });
+    test("draw with scissors", () => {
+      expect(rockPaperScissors(["C Z"])).toEqual(6);
+    });
+    test("lose with rock", () => {
+      expect(rockPaperScissors(["B X"])).toEqual(1);
+    });
+    test("win with rock", () => {
+      expect(rockPaperScissors(["C X"])).toEqual(7);
+    });
   });
-  test("draw with paper", () => {
-    expect(rockPaperScissors(["B Y"])).toEqual(5);
-  });
-  test("draw with scissors", () => {
-    expect(rockPaperScissors(["C Z"])).toEqual(6);
-  });
-  test("lose with rock", () => {
-    expect(rockPaperScissors(["B X"])).toEqual(1);
-  });
-  test("win with rock", () => {
-    expect(rockPaperScissors(["C X"])).toEqual(7);
+
+  describe("multiple rounds", () => {
+    test("score mutliple rounds", () => {
+      expect(rockPaperScissors(["A X", "B Y", "C Z"])).toEqual(15);
+    });
+    test("score mutliple rounds", () => {
+      expect(rockPaperScissors(["A Y", "B X", "C Z"])).toEqual(15);
+    });
+    test("score mutliple rounds", () => {
+      expect(
+        rockPaperScissors([
+          "C X",
+          "C Y",
+          "C X",
+          "B X",
+          "B Z",
+          "A Z",
+          "C Y",
+          "C Z",
+          "B Z",
+          "C X",
+          "B Y",
+          "C Y",
+          "C Y",
+          "A Y",
+          "C Y",
+          "C Y",
+          "C Z",
+          "C X",
+          "B Z",
+          "C Y",
+          "A Y",
+          "A Y",
+          "C Z",
+          "B Y",
+          "A Y",
+          "C Z",
+          "C Y",
+          "A Y",
+          "A Y",
+          "B Y",
+          "C Y",
+          "C Z",
+        ])
+      ).toEqual(170);
+    });
   });
 });
 
-describe("multiple rounds", () => {
-  test("score mutliple rounds", () => {
-    expect(rockPaperScissors(["A X", "B Y", "C Z"])).toEqual(15);
+describe("part 2", () => {
+  describe("Single round", () => {
+    test("Rock and draw", () => {
+      expect(fullStrategy(["A Y"])).toEqual(4);
+    });
+    test("Rock and win", () => {
+      expect(fullStrategy(["A Z"])).toEqual(8);
+    });
   });
-  test("score mutliple rounds", () => {
-    expect(rockPaperScissors(["A Y", "B X", "C Z"])).toEqual(15);
-  });
-  test("score mutliple rounds", () => {
-    expect(
-      rockPaperScissors([
-        "C X",
-        "C Y",
-        "C X",
-        "B X",
-        "B Z",
-        "A Z",
-        "C Y",
-        "C Z",
-        "B Z",
-        "C X",
-        "B Y",
-        "C Y",
-        "C Y",
-        "A Y",
-        "C Y",
-        "C Y",
-        "C Z",
-        "C X",
-        "B Z",
-        "C Y",
-        "A Y",
-        "A Y",
-        "C Z",
-        "B Y",
-        "A Y",
-        "C Z",
-        "C Y",
-        "A Y",
-        "A Y",
-        "B Y",
-        "C Y",
-        "C Z",
-      ])
-    ).toEqual(170);
+  describe("Multiple round", () => {
+    test("", () => {
+      expect(fullStrategy(["A X", "B Y", "C Z"])).toEqual(3 + 5 + 8);
+    });
   });
 });
