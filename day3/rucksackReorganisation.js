2764a7700169705011843b92050e0b8fb28ba019
Curtis Turk <curtis.c.turk@outlook.com>
2022-12-07 12:05:34 +0000

    Completed part 2

--- day3/rucksackReorganisation.js
+++ day3/rucksackReorganisation.js
@@ -1,3 +1,4 @@
+const { group } = require("console");
 const fs = require("fs");
 
 const inputData = fs.readFileSync("./input.txt", "utf-8").split("\n");
@@ -9,7 +10,7 @@ const getSharedItems = (rucksack) => {
     .slice(rucksack.length / 2, rucksack.length)
     .split("");
   return [
-    ...new Set(firstSection.filter((value) => secondSection.includes(value))),
+    ...new Set(firstSection.filter((char) => secondSection.includes(char))),
   ];
 };
 
@@ -25,24 +26,48 @@ const calculatePriority = (letter) => {
 const testInputData = fs.readFileSync("./testInput.txt", "utf-8").split("\n");
 
 const totalTest = testInputData
-  .map((rucksack) => {
-    return getSharedItems(rucksack);
-  })
+  .map((rucksack) => getSharedItems(rucksack))
   .map((letter) => calculatePriority(letter))
   .reduce((a, b) => a + b);
 
-console.log(totalTest);
-
 // Real Data
-
 const total = inputData
   .map((rucksack) => getSharedItems(rucksack))
   .map((letter) => calculatePriority(letter))
   .reduce((a, b) => a + b);
 
+// Part 2
+
+const makeGroupsOfThree = (data) => {
+  const groupsOfThree = [];
+  for (let i = 0; i < data.length; i += 3) {
+    let group = [data[i], data[i + 1], data[i + 2]];
+    groupsOfThree.push(group);
+  }
+  return groupsOfThree;
+};
+
+const getGroupBadge = (group) => {
+  let one = group[0].split("");
+  let two = group[1].split("");
+  let three = group[2].split("");
+
+  return [
+    ...new Set(
+      one.filter((char) => two.includes(char) && three.includes(char))
+    ),
+  ];
+};
+
+// Test Data
+let groupTotal = makeGroupsOfThree(inputData)
+  .map((group) => getGroupBadge(group))
+  .map((badge) => calculatePriority(badge))
+  .reduce((a, b) => a + b);
+
 // Part 1
 console.log(total);
-
-// console.log(calculatePriority(sharedArray));
+// Part 2
+console.log(groupTotal);
 
 module.exports = { getSharedItems, calculatePriority };
